12f1ae96a4d016362c8b678ac7b619bba277cde8
Drahonovsky226382 <drahonovsky.matous@gmail.com>
2022-03-29 09:44:41 +0200

    uz me to nebavi

--- prace_s_GIT.py
+++ prace_s_GIT.py
@@ -1,4 +1,4 @@
-def is_whole_number(number):
+def is_natural_number(number):
     if number % 1 == 0 and number > 0:
         return True
     return False
@@ -9,10 +9,12 @@ def is_even_number(number):
         return True
     return False
 
+def is_natural_number():
+    for my_number in my_numbers:
+        print(my_number)
 
 
-s = 0
 
-my_number = 9
-print(is_whole_number(my_number))
-print(is_even_number(my_number))
+my_numbers = [2, 4, 8, 9, 10]
+print(is_natural_number(my_numbers))
+print(is_even_number(my_numbers))
