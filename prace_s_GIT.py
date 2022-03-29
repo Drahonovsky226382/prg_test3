747cedcea1dc0d4551538fd740a660841d633b1a
Drahonovsky226382 <drahonovsky.matous@gmail.com>
2022-03-29 09:06:25 +0200

    treti commit hotovy

--- prace_s_GIT.py
+++ prace_s_GIT.py
@@ -1,8 +1,14 @@
 def is_whole_number(number):
     if number % 1 == 0 and number > 0:
         return True
+    return False
 
+
+def is_even_number(number):
+    if number % 2 == 0:
+        return True
     return False
 
 my_number = 12
 print(is_whole_number(my_number))
+print(is_even_number(my_number))
